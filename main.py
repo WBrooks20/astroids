65398ccc16860faeb84c89324fce16f65e5fbd8c
Alvarr201 <wzbrooks1@gmail.com>
2024-11-29 20:57:08 -0700

    added logic for player movement. added player class obj to pygame groups.

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ def main():
     #imported from constants.py
     player_start_x = SCREEN_WIDTH / 2
     player_start_y = SCREEN_HEIGHT / 2
+    #Create the pygame groups and add the groups to a containers class variable on the Player class.
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.Player.containers = (updateable,drawable)
+    
     player1 = player.Player(player_start_x,player_start_y)
+    
     print("Starting asteroids!")
     print(f"""
           Screen width: {SCREEN_WIDTH}
@@ -23,7 +29,13 @@ def main():
                 return
         #Fill in the background on frame refresh with a black screen.
         screen.fill("black")
-        player1.draw(screen)
+
+        for object in updateable:
+            object.update(dt)
+        
+        for object in drawable:
+            object.draw(screen)
+            
         #Frame update.
         pygame.display.flip()
         #Set FPS to 60. DT is the detla time (time since last refresh) in seconds.
